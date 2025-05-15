9425a961c4470dd92ea7e12e9f08cb91e0ca1530
vijji <vijji@Vijaya>
2025-05-15 16:47:47 +0530

    Demo1 class committed.

---
+++ src/test/java/sample/Demo1.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		add(10,20);
+	}
+		
+		static void add(int a, int b) {
+			int c = a+b;
+			System.out.println(c);
+		}
+
+	}
